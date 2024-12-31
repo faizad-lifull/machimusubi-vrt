c96fcedfe954021a1421ae53827abb0bff20b118
faizad-lifull <norfaizad@lifull.com>
2024-12-31 12:44:18 +0800

    feat: Refine

--- tests/src/vrt/locators.ts
+++ tests/src/vrt/locators.ts
@@ -20,37 +20,37 @@ export const pagePatterns: {
   //   deviceType: ["sp", "pc"],
   //   describe: "Lifestyle Station Page Test",
   // },
-  {
-    path: "/machimusubi/tokyo/lifestyle/64/",
-    deviceType: ["sp", "pc"],
-    describe: "Lifestyle Id Page Test",
-  },
-  {
-    path: "/machimusubi/tokyo/shinagawa_00224-st/",
-    deviceType: ["sp"],
-    maskingSelectors: [
-      // Mask user review bubbles
-      "#prg-areaReviews > div.reviewContent > div.reviewList > dl > dd",
-      // Mask user icons and demographic information
-      "#prg-areaReviews > div.reviewContent > div.reviewList > dl > dt",
-    ],
-    describe: "Station Detail Page Test",
-  },
-  {
-    path: "/machimusubi/gifu/line/",
-    deviceType: ["sp", "pc"],
-    describe: "Line List Page Test",
-  },
-  {
-    path: "/machimusubi/gifu/chuohonsen-line/",
-    deviceType: ["sp", "pc"],
-    describe: "Line Page Test",
-  },
-  {
-    path: "/machimusubi/not/found/",
-    deviceType: ["sp", "pc"],
-    describe: "Error Page Test",
-  },
+  // {
+  //   path: "/machimusubi/tokyo/lifestyle/64/",
+  //   deviceType: ["sp", "pc"],
+  //   describe: "Lifestyle Id Page Test",
+  // },
+  // {
+  //   path: "/machimusubi/tokyo/shinagawa_00224-st/",
+  //   deviceType: ["sp"],
+  //   maskingSelectors: [
+  //     // Mask user review bubbles
+  //     "#prg-areaReviews > div.reviewContent > div.reviewList > dl > dd",
+  //     // Mask user icons and demographic information
+  //     "#prg-areaReviews > div.reviewContent > div.reviewList > dl > dt",
+  //   ],
+  //   describe: "Station Detail Page Test",
+  // },
+  // {
+  //   path: "/machimusubi/gifu/line/",
+  //   deviceType: ["sp", "pc"],
+  //   describe: "Line List Page Test",
+  // },
+  // {
+  //   path: "/machimusubi/gifu/chuohonsen-line/",
+  //   deviceType: ["sp", "pc"],
+  //   describe: "Line Page Test",
+  // },
+  // {
+  //   path: "/machimusubi/not/found/",
+  //   deviceType: ["sp", "pc"],
+  //   describe: "Error Page Test",
+  // },
   {
     path: "/machimusubi/tokyo/heiwadai_06376-st/",
     deviceType: ["pc"],
